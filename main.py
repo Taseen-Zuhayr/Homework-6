9a944f3b840458dec54d6f3a8c398229f50eacf2
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-21 12:12:24 +0600

    Homework 6

---
+++ main.py
@@ -0,0 +1,56 @@
+a = input("Enter a alphabet or a sign here : ")
+
+if a == "A" or a == "a":
+    print("It is an alphabet.")
+elif a == "B" or a == "b":
+    print("It is an alphabet.")
+elif a == "C" or a == "c":
+    print("It is an alphabet.")
+elif a == "D" or a == "d":
+    print("It is an alphabet.")
+elif a == "E" or a == "e":
+    print("It is an alphabet.")
+elif a == "F" or a == "f":
+    print("It is an alphabet.")
+elif a == "G" or a == "g":
+    print("It is an alphabet.")
+elif a == "H" or a == "h":
+    print("It is an alphabet.")
+elif a == "I" or a == "i":
+    print("It is an alphabet.")
+elif a == "J" or a == "j":
+    print("It is an alphabet.")
+elif a == "K" or a == "k":
+    print("It is an alphabet.")
+elif a == "L" or a == "l":
+    print("It is an alphabet.")
+elif a == "M" or a == "m":
+    print("It is an alphabet.")
+elif a == "N" or a == "n":
+    print("It is an alphabet.")
+elif a == "O" or a == "o":
+    print("It is an alphabet.")
+elif a == "P" or a == "p":
+    print("It is an alphabet.")
+elif a == "Q" or a == "q":
+    print("It is an alphabet.")
+elif a == "R" or a == "r":
+    print("It is an alphabet.")
+elif a == "S" or a == "s":
+    print("It is an alphabet.")
+elif a == "T" or a == "t":
+    print("It is an alphabet.")
+elif a == "U" or a == "u":
+    print("It is an alphabet.")
+elif a == "V" or a == "v":
+    print("It is an alphabet.")
+elif a == "W" or a == "w":
+    print("It is an alphabet.")
+elif a == "X" or a == "x":
+    print("It is an alphabet.")
+elif a == "Y" or a == "y":
+    print("It is an alphabet.")
+elif a == "Z" or a == "z":
+    print("It is an alphabet.")
+else:
+    print("It is NOT an alphabet.")
